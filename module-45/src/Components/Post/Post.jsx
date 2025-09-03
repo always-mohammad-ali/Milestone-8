ac7448c4ce6f169e6eea2dcf904a4435c5e3cac3
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-03 18:51:07 +0600

    navigate

--- module-45/src/Components/Post/Post.jsx
+++ module-45/src/Components/Post/Post.jsx
@@ -1,13 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
     const {id, title} = post;
+    
+    const navigate = useNavigate();
+
+
+    const handleDetails = () =>{
+        navigate(`/post/${id}`)
+    }
     return (
         <div>
             <div className='border'>
                <h3>USER: {id}</h3>
                <p>{title}</p>
                <Link to={`/post/${id}`}>Details</Link>
+               <button onClick={handleDetails}>Alternative way for see Detalis</button>
             </div>
             
         </div>
--- module-45/src/Components/PostDetail/PostDetail.jsx
+++ module-45/src/Components/PostDetail/PostDetail.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetail = () => {
     const postDetails = useLoaderData();
     const {id, title, body} = postDetails;
+
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1)
+    }
     return (
         <div>
             <p>{id}</p>
@@ -11,6 +16,8 @@ const PostDetail = () => {
             <h3>
                 {body}
             </h3>
+
+            <button className='border' onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
